396fff5240ed5840ba5713c194a53ac84a03d9c3
abhishek4041 <sharma.abhishek4041@gmail.com>
2025-03-10 05:19:42 -0500

    updated lab files

---
+++ ex_01_Java_Basic/Lab003_Comments.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basic;
+
+public class Lab003_Comments {
+
+    public static void main(String[] args){
+
+        /**
+         * Author: Abhishek sharma
+         * Creating multiple line comments
+         * using java
+         */
+
+        System.out.println("The demo comment");
+    }
+
+
+}
---
+++ ex_01_Java_Basic/Lab007_MathOperations.java
@@ -0,0 +1,20 @@
+package ex_01_Java_Basic;
+
+public class Lab007_MathOperations {
+
+    public static void main(String[] args){
+
+        int a = 10;
+        int b = 7;
+
+        System.out.println("a plus b:" + (a+b));
+
+        System.out.println("a minus b:" + (a-b));
+
+        System.out.println("a multiplies b:" + a*b);
+
+        System.out.println("a divides b:" + a/b);
+
+        System.out.println("a percentile b:" + a%b);
+    }
+}

6bd20491755db96f790c9b9b6b2141c4614b526a
abhishek4041 <sharma.abhishek4041@gmail.com>
2025-03-20 18:11:30 -0500

    added assignment

--- ex_06_switch/Lab02_Switch.java
+++ ex_06_switch/Lab02_Switch.java
@@ -1,2 +1,28 @@
-package ex_06_switch;public class Lab02_Switch {
+package ex_06_switch;
+
+public class Lab02_Switch {
+
+    public static void main(String[] args){
+
+        int a = 11;
+
+        switch(-1){
+
+            case 1:
+
+                System.out.println("hello");
+                break;
+
+            case 2:
+                System.out.println("Hello 2");
+                break;
+
+            case -1:
+                System.out.println("Hello -1");
+                break;
+
+            default:
+                System.out.println("default");
+        }
+    }
 }

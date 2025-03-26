837a58b3579d2a2ff42f0d2e20ba1c0f918b04d9
abhishek4041 <sharma.abhishek4041@gmail.com>
2025-03-25 19:15:13 -0500

    vowels and consonents

--- task/Task09_Palindrome.java
+++ task/Task09_Palindrome.java
@@ -1,4 +1,34 @@
 package task;
 
+import java.util.Scanner;
+
 public class Task09_Palindrome {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        scanner.close();
+
+        if (isPalindrome(number)) {
+            System.out.println("The number is a palindrome.");
+        } else {
+            System.out.println("The number is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(int num) {
+        int original = num, reversed = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        return original == reversed;
+    }
 }

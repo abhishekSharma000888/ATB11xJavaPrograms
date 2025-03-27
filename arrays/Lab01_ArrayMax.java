21743152152bb8b0859f69606340b8d61b3e2c66
abhishek4041 <sharma.abhishek4041@gmail.com>
2025-03-26 21:05:09 -0500

    array max

---
+++ arrays/Lab01_ArrayMax.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class Lab01_ArrayMax {
+
+    public static void main(String[] args){
+
+        int[] arr = {1,3,4,5,2,7,8,9,45};
+
+        int max = arr[0];
+
+
+        for(int i=0; i<arr.length; i++){
+
+            if(max < arr[i]){
+
+                max = arr[i];
+            } 
+
+        }
+
+        System.out.println(max);
+    }
+}

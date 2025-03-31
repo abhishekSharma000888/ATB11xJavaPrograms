44e0faba523f7880293e05e5c2ed1a98e3135098
abhishek4041 <sharma.abhishek4041@gmail.com>
2025-03-31 02:10:07 -0500

    PersonClass

---
+++ ex_08_oopsConcepts/Lab01_OOPS.java
@@ -0,0 +1,20 @@
+package ex_08_oopsConcepts;
+
+import java.security.KeyStore;
+
+public class Lab01_OOPS {
+
+    public static void main(String[] args){
+
+        String name = "Alpha";
+
+        String phone = "Samsung";
+
+        byte age = 30;
+
+        float Height;
+
+        float weight;
+
+    }
+}

69d5c21f9c4d41bb42c82e3b7c0260aebc546cec
abhishek4041 <sharma.abhishek4041@gmail.com>
2025-04-10 20:17:38 -0500

    exceptions

---
+++ ex_13_exceptions/Lab02_StringNullPointerExceptionExample.java
@@ -0,0 +1,11 @@
+package ex_13_exceptions;
+
+public class Lab02_StringNullPointerExceptionExample {
+
+    public static void main(String[] args){
+
+        String a = null;
+
+        System.out.println(a.trim()); // unchecked string 
+    }
+}

774cee75c818258d3664c5d9c06af1645549ff4c
abhishek4041 <sharma.abhishek4041@gmail.com>
2025-04-20 21:20:58 -0500

    definitions

---
+++ task/Task24_Declaration.java
@@ -0,0 +1,45 @@
+package task;
+
+public class Task24_Declaration {
+
+// Contains definitions for HTTP, client-server, authentication and authorization, cookies, etc.
+
+// HTTP
+/*
+Stands for HyperText Transfer Protocol.
+
+It provides a unique URL to get specific responses related to the requests made by the client.
+*/
+
+// Client -> Server
+/*
+Client is the user or system that sends a request to the server.
+
+The server handles the incoming requests and sends appropriate responses back to the client.
+*/
+
+// Authentication and Authorization
+/*
+Authentication is the process of verifying the identity of a user trying to access the application.
+
+Authorization is the process of granting specific permissions to a user after successful authentication.
+*/
+
+// Cookie
+/*
+Used to track user behavior and maintain state across different pages or sessions on the web.
+*/
+
+// Header
+/*
+Acts as a metadata container for HTTP requests and responses. It can include information such as content type, status, authorization tokens, etc.
+*/
+
+// URL vs URI
+/*
+URI (Uniform Resource Identifier) is a broader term that identifies a resource, either by location, name, or both.
+
+URL (Uniform Resource Locator) is a specific type of URI that provides the exact address of a resource on the internet.
+*/
+
+}
